8904a706fc41af2245d55c075e492f19204d4ce2
Ola-Salawu Olawale <127776716+Wale2007@users.noreply.github.com>
2026-07-23 14:52:58 +0000

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Reviews from './components/Reviews';
 import Contact from './components/Contact';
